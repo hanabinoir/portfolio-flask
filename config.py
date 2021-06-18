1760a5f1a91c565b0cf8c1bff69f94e69c09c5ac
Vincent Shen <crigshn@gmail.com>
2021-06-18 19:54:10 +0900

    DB URI updated

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -8,20 +9,16 @@ load_dotenv()
 class Config:
     DEBUG = True
 
-    MONGO_URI = 'mongodb+srv://{usr}:{pwd}@{host}/{db}?' \
-                'retryWrites=true&w=majority'\
-        .format(**{
-            'usr': os.getenv('MONGO_USR'),
-            'pwd': os.getenv('MONGO_PWD'),
-            'host': os.getenv('MONGO_HOST'),
-            'db': os.getenv('MONGO_DB'),
-        })
+    MONGO_URI = f"mongodb+srv://{os.getenv('MONGO_USR')}:" \
+                f"{os.getenv('MONGO_PWD')}@" \
+                f"{os.getenv('MONGO_HOST')}/" \
+                f"{os.getenv('MONGO_DB')}?" \
+                'retryWrites=true&w=majority'
+    logging.info(MONGO_URI)
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{usr}:{pwd}@{host}/{db}' \
-        .format(**{
-            'usr': os.getenv('MYSQL_USR'),
-            'pwd': os.getenv('MYSQL_PWD'),
-            'host': os.getenv('MYSQL_HOST'),
-            'db': os.getenv('MYSQL_DB'),
-        })
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USR')}:" \
+                              f"{os.getenv('MYSQL_PWD')}@" \
+                              f"{os.getenv('MYSQL_HOST')}/" \
+                              f"{os.getenv('MYSQL_DB')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    logging.info(SQLALCHEMY_DATABASE_URI)
